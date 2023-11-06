407b68e6f4f379bdb89b2df04f58916af005ce77
D.H.D. Nguyen <jasmine.1995dn@gmail.com>
2023-11-06 21:19:18 +0100

    update arguments"

--- arguments.py
+++ arguments.py
@@ -6,7 +6,7 @@ def k_parser():
 	parser = ArgumentParser(description="This contains script for KAPING pipeline")
 
 	parser.add_argument("--input", help="Input json file")
-	parser.add_argument("--output", help="Output file")
+	parser.add_argument("--output", default="./mintaka_predicted.csv" help="Output file, default: ./mintaka_predicted.csv")
 
 	parser.add_argument('--k', type=int, default=10, help="number of triples to retrieve (default: 10)")
 	parser.add_argument('--random', action='store_true', help="call this flag to use the baseline of random knowledge instead of KAPING")
